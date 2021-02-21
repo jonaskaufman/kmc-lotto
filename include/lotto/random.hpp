2d2c7a31e01a5e3e1f238eb23e46f2c15bc4254e
Jonas Kaufman <jonas.l.kaufman@gmail.com>
2021-02-21 13:53:01 -0800

    Made reseeding random generator public, switched int type to uint_fast64_t

--- include/lotto/random.hpp
+++ include/lotto/random.hpp
@@ -1,19 +1,24 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
+#include <cstdint>
 #include <random>
+
+using UIntType = std::uint_fast64_t;
 
 namespace lotto
 {
 /**
  * Random number generator
  *
- * Allows sampling of random integers and reals.
+ * Allows sampling of random integers and reals using
+ * the Mersenne Twister 19937 generator (64-bit)
  */
 class RandomGenerator
 {
+
 public:
-    /// Default constructor, automatically seeds generator
+    /// Default constructor, automatically seeds generator from random device
     RandomGenerator()
     {
         std::random_device device;
@@ -21,23 +26,32 @@ public:
     }
 
     /// Returns a random integer between 0 and maximum_value (inclusive)
-    int sample_integer_range(int maximum_value)
+    UIntType sample_integer_range(UIntType maximum_value)
     {
-        return std::uniform_int_distribution<int>(0, maximum_value)(generator);
+        return std::uniform_int_distribution<UIntType>(0, maximum_value)(generator);
     }
 
-    /// Returns a random real from the unit interval
+    /// Returns a random real from the unit interval (includes 0, excludes 1)
     double sample_unit_interval() { return std::uniform_real_distribution<double>(0.0, 1.0)(generator); }
+
+    /// Returns the value used to seed the generator
+    UIntType get_seed() const { return seed; }
+
+    /// Reseeds the generator
+    void reseed_generator(UIntType new_seed)
+    {
+        seed = new_seed;
+        generator.seed(seed);
+    }
 
 private:
     /// 64-bit Mersenne Twister generator
     std::mt19937_64 generator;
 
-    /// Reseeds the generator
-    void reseed_generator(unsigned int seed) { generator = std::mt19937_64(seed); }
+    /// Seed used for generator
+    UIntType seed;
 
-    //    /// Uniform real distribution on unit interval
-    //    std::uniform_real_distribution<double> uniform_unit_interval_distribution;
+    // TODO: Store distributions as members?
 };
 } // namespace lotto
 
