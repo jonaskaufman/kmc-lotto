3261166a368b3574ee172a91e9d074f204ee3857
Jonas Kaufman <jonas.l.kaufman@gmail.com>
2021-03-23 18:07:36 -0700

    Wrote EventRateTree class interface

--- include/lotto/event_rate_tree.hpp
+++ include/lotto/event_rate_tree.hpp
@@ -1,6 +1,8 @@
 #ifndef EVENT_RATE_TREE_H
 #define EVENT_RATE_TREE_H
 
+#include "sum_tree.hpp"
+#include <map>
 #include <optional>
 
 namespace lotto
@@ -11,7 +13,7 @@ namespace lotto
  * For non-leaves, should only contain a (summed) rate
  */
 template <typename EventIDType>
-class EventRateNode
+class EventRateNode // rename to node data or something?
 {
 public:
     // Construct as a leaf given an event ID and rate
@@ -50,6 +52,50 @@ private:
 
     // Event rate or summed rate
     double rate;
+};
+
+/*
+ * Tree of event rates, with events as leaves
+ */
+template <typename EventIDType>
+class EventRateTree
+{
+public:
+    // Construct tree given list of event IDs and corresponding initial rates
+    EventRateTree(const std::vector<EventIDType>& all_event_ids, const std::vector<double>& all_rates);
+
+    // Traverse tree and return the event ID of event at index i
+    // for which R(i-1) < u <= R(i), where u is the query value
+    // and R(i) is cumulative rate of all events up to and including event i
+    const EventIDType& query_tree(double query_value) const;
+    // Should this take scale for total rate automatically?
+
+    // Update the rate of a specific event
+    void update_rate(const EventIDType& event_id, double new_rate);
+
+    // Return the total rate of all events stored in tree
+    double total_rate() const;
+
+private:
+    using NodeData = EventRateNode<EventIDType>;
+    using Node = InvertedBinaryTreeNode<NodeData>;
+    using Index = int;
+
+    //
+    InvertedBinarySumTree<NodeData> event_rate_tree;
+
+    //
+    const std::map<EventIDType, Index> event_to_leaf_index; // needed for updating tree itself given an EventID
+
+    //
+    std::vector<NodeData> events_as_leaves(const std::vector<EventIDType>& init_events,
+                                           const std::vector<double>& init_rates);
+
+    //
+    std::map<EventIDType, Index> event_to_leaf_index_map() const;
+
+    //
+    const Node* bifurcate(const Node* current_node_ptr, double& running_rate) const;
 };
 
 } // namespace lotto
